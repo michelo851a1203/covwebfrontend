dc28e62c28b8544c87679ea1e41a77eb4e754456
MichaelHo <michelo851a1203@gmail.com>
2020-08-25 01:11:45 +0800

    20200824 fix vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -10,30 +10,56 @@ const routes = [
       {
         path: '/',
         component: () => import(/* webpackChunkName: "Index" */ '../views/home/Index.vue'),
-        meta: { authRequired: true }
+        meta: { authRequired: true, noDirect: true }
       },
       {
         path: '/report',
         component: () => import(/* webpackChunkName: "Report" */ '../views/home/Report.vue'),
-        meta: { authRequired: true }
+        meta: { authRequired: true, noDirect: true }
       },
     ]
   },
   {
     path: '/signIn',
     name: 'signIn',
-    component: () => import(/* webpackChunkName: "about" */ '../views/SignIn.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/SignIn.vue'),
+    meta: { noDirect: true }
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+    meta: { noDirect: true }
+  },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.noDirect) {
+    next({
+      name: "signIn"
+    })
+    return
+  }
+  if (!to.meta.authRequired) {
+    next();
+    return;
+  }
+
+  const cluster = localStorage.getItem("covWebItem")
+  const token = localStorage.setItem(cluster)
+  if (cluster && cluster !== "" && token && token !== "") {
+    next()
+    return
+  }
+  next({
+    name: "signIn"
+  })
+})
+
 
 export default router
