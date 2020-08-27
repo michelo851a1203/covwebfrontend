eb3747e06b150eccd3bc4e847ae20d4daca2338c
MichaelHo <michelo851a1203@gmail.com>
2020-08-27 09:36:29 +0800

    20200827 - add loading status

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -5,7 +5,27 @@
         <recordlist></recordlist>
       </template>
       <template #fallback>
-        <div>loading...</div>
+        <div class="flex items-center px-6 py-4">
+          <div>
+            <svg
+              class="boardloading stroke-current text-green-600"
+              width="100"
+              viewBox="0 0 100 100"
+            >
+              <circle
+                cx="50"
+                cy="50"
+                r="25"
+                stroke-dasharray="157"
+                stroke-dashoffset="0"
+                stroke-linecap="round"
+                stroke-width="10"
+                fill="transparent"
+              />
+            </svg>
+          </div>
+          <div class="ml-10">loading...</div>
+        </div>
       </template>
     </suspense>
   </div>
@@ -50,5 +70,37 @@ export default {
   .recordContent {
     width: 20%;
   }
+}
+
+@keyframes Loading {
+  0% {
+    stroke-dashoffset: 0;
+  }
+
+  75% {
+    stroke-dashoffset: 250;
+  }
+
+  100% {
+    stroke-dashoffset: 314;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.boardloading {
+  animation: rotate 0.6s linear infinite;
+}
+
+.boardloading circle {
+  animation: Loading 2.4s linear infinite;
 }
 </style>
