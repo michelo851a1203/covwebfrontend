89300f2a806794284964f4c96803c405cf62e0fe
MichaelHo <michelo851a1203@gmail.com>
2020-08-28 09:42:36 +0800

    20200828M - fix

--- src/components/ScanQrcode.vue
+++ src/components/ScanQrcode.vue
@@ -42,7 +42,8 @@ export default {
       mainFrame.value.src = frameSrc;
       window.onmessage = async function (e) {
         console.group(`%c eData`, "color:yellow");
-        console.log(e.Data);
+        console.log(e);
+        console.log(e.data);
         console.groupEnd();
         if (e.Data && e.Data !== "") {
           verificationModule.verifyCredentialId.value = e.Data;
