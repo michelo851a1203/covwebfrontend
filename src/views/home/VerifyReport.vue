e9aa5fff10f8e55fd2915d7bfaefe74637f74cc4
MichaelHo <michelo851a1203@gmail.com>
2020-09-02 17:40:38 +0800

    20200902 E append Testresult

--- src/views/home/VerifyReport.vue
+++ src/views/home/VerifyReport.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="verifyContent py-12 sm:mt-12 sm:shadow-2xl sm:rounded mx-auto sm:bg-white">
+    <div
+      v-if="proofAttribute['Test Result'] && (proofAttribute['Test Result'] === 'DETECTED' || proofAttribute['Test Result'] === 'NONDETECTED')"
+      class="flex items-center justify-center mb-12"
+    >
+      <div>
+        <h2 class="text-xl font-bold">Test Result</h2>
+        <p
+          v-if="proofAttribute['Test Result'] === 'NONDETECTED'"
+          class="text-lg text-green-600 font-bold"
+        >
+          NEGATIVE
+          <br />Vaild
+        </p>
+        <p v-else class="text-lg text-red-600 font-bold">
+          POSITIVE
+          <br />Outdate
+        </p>
+      </div>
+      <div class="ml-12 sm:ml-20">
+        <svg
+          v-if="proofAttribute['Test Result'] === 'NONDETECTED'"
+          viewBox="0 0 100 100"
+          class="w-16 stroke-current text-green-600"
+        >
+          <polyline
+            stroke-width="6"
+            stroke-linejoin="round"
+            fill="transparent"
+            points="10,10 90,10 90,90 10,90 10,10"
+          />
+          <polyline
+            points="25,50 40,75 75,25"
+            stroke-width="6"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            fill="transparent"
+          />
+        </svg>
+        <svg v-else viewBox="0 0 100 100" class="w-16 stroke-current text-red-600">
+          <polyline
+            stroke-width="6"
+            stroke-linejoin="round"
+            fill="transparent"
+            points="10,10 90,10 90,90 10,90 10,10"
+          />
+          <line stroke-width="6" stroke-linecap="round" x1="30" y1="30" x2="70" y2="70" />
+          <line stroke-width="6" stroke-linecap="round" x1="30" y1="70" x2="70" y2="30" />
+        </svg>
+      </div>
+    </div>
     <section class="flex items-center justify-around">
       <div class="py-2">
         <table class="text-sm">
