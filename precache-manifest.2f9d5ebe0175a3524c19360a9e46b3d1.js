6ee9d5fbbe1206cb15871627142a59849f1f6aac
MichaelHo <michelo851a1203@gmail.com>
2020-09-07 16:14:59 +0800

    deploy

---
+++ precache-manifest.2f9d5ebe0175a3524c19360a9e46b3d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1120fcb7ddf88d459389bc4890b3fc",
+    "url": "/covwebfrontend/index.html"
+  },
+  {
+    "revision": "4cd5a67cf0bede4211ea",
+    "url": "/covwebfrontend/js/Index.a3ae44b6.js"
+  },
+  {
+    "revision": "c059bead1e6ade5b7c49",
+    "url": "/covwebfrontend/js/Report.49a7b20a.js"
+  },
+  {
+    "revision": "dcc8978730c0143ab4aa",
+    "url": "/covwebfrontend/js/Report~Scan.6826d8b2.js"
+  },
+  {
+    "revision": "c61ccbba4353774c031a",
+    "url": "/covwebfrontend/js/Scan.eaa6dbec.js"
+  },
+  {
+    "revision": "1ca2d94187c7eb061afe",
+    "url": "/covwebfrontend/js/VerifyReport.3ac6a11c.js"
+  },
+  {
+    "revision": "9041778764b69582d40c",
+    "url": "/covwebfrontend/js/about.a7348fc3.js"
+  },
+  {
+    "revision": "cfa837714c48a2a9fd42",
+    "url": "/covwebfrontend/js/app.c457a369.js"
+  },
+  {
+    "revision": "50c65544e5d3251ad945",
+    "url": "/covwebfrontend/js/chunk-vendors.d2c29c0e.js"
+  },
+  {
+    "revision": "ad813cf3ae2ec207486f5526d906f967",
+    "url": "/covwebfrontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covwebfrontend/robots.txt"
+  }
+]);
